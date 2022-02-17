c46c056641af6f5f4cfa2f89150abc89bb383c69
Wizty79 <wistikwistisen@gmail.com>
2022-02-17 14:06:22 +0000

    Trello Edit

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config
+import requests 
+import dotenv 
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -9,7 +12,28 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
+
+    url = "https://api.trello.com/1/boards/6205664a19d7b437223061eb/lists"
+
+    dotenv.load_dotenv(".env")
+
+    print(os.getenv("API_KEY"))
+
+    querystring = {
+        "key":os.getenv("API_KEY"),
+        "token":os.getenv("API_TOKEN"),
+        "cards": "open"
+    }
+
+    response - requests.request("GET", url, params=querystring)
+
+    response_json - response.jason()
+
+    for trello_list in response_json:
+        for card in trello_list['cards']:
+            card['status'] - trello_list['name']
+
+    items = response_json[0]['cards']
     return render_template('index.html', items = items)
 
 @app.route('/create-todo', methods=['Post'])
