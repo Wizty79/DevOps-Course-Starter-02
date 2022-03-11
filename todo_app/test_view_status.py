72deabf858f945d282aeecc5bbaa6d992bd46c50
Wizty79 <wistikwistisen@gmail.com>
2022-03-11 13:00:34 +0000

    first test

---
+++ todo_app/test_view_status.py
@@ -0,0 +1,14 @@
+import unittest
+
+#Arrenge -  Create an instance of ViewModel containing some example Item objects with various statuses
+
+def view_test():
+    status_1 = 'Doing'
+    status_2 = 'Done'
+
+
+#Act -  Get the result of the view model's new doing_items property (though this will actually be an empty list for now).
+
+
+#Assert - Write one or more assert statements checking the result of the "Act" step equals what you want
+
