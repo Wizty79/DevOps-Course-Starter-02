ead387b88cc54e77339584e384456347a326937b
Wizty79 <wistikwistisen@gmail.com>
2022-03-25 14:59:53 +0000

    chaos edit

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -7,6 +7,13 @@ class ViewModel:
 		return self._items
 		
 	@property
-	def todo_items(self):
-		return []
+	def todo_items(self): 
+        
+    	todo_list_items = []
+                
+    	for item in self.items: 
+            if status == 'To Do':   
+	            print (todo_list_items.append(item)) 
+            
+    	return todo_list_items
 	
