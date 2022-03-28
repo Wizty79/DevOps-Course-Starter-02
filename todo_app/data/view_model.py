1b3ac5ef2d00a7e32ce0cc28e602514cc82ccab6
Wizty79 <wistikwistisen@gmail.com>
2022-03-28 13:37:32 +0000

    Alex Session Edit

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -1,20 +1,21 @@
 class ViewModel:
-	def __init__(self, items):
-		self._items = items
+    def __init__(self, items):
+        self._items = items
  
-	@property
-	def items(self):
-		return self._items
-		
-	@property
-	def todo_items(self): 
+    @property
+    def items(self):
+        return self._items
         
-    	todo_list_items = []
-		self.status from items = status = self.status#? or status = self.status
+    @property
+    def todo_items(self): 
+        todo_list_items = []
                 
-    	for item in self.items: 
-            if status == 'To Do':
-	    	todo_list_items.append(item)#? 
+        for item in self.items: 
+            if item.status == 'To Do':
+                todo_list_items.append(item)
             
-    	return todo_list_items
-	
+        return todo_list_items
+
+    @property
+    def done_items(self): 
+        return []
--- todo_app/test_view_model.py
+++ todo_app/test_view_model.py
@@ -4,7 +4,6 @@ from todo_app.data.item import Item
 # Requirements for todo_items property
 # - Status, if Todo keep, if not Todo throw away
 
-
 def test_todo_items_property_only_shows_todo_items_and_not_anything_else():
     #Arrange -  Create an instance of ViewModel containing some example Item objects with various statuses
     items = [
@@ -16,7 +15,7 @@ def test_todo_items_property_only_shows_todo_items_and_not_anything_else():
     view_model = ViewModel(items)
 
 
-    #Act -  Get the result of the view model's new doing_items property (though this will actually be an empty list for now).
+    #Act -  Get the result of the view model's new todo_items property (though this will actually be an empty list for now).
     test_todo_items = view_model.todo_items
 
     assert len(test_todo_items) == 1
@@ -27,3 +26,24 @@ def test_todo_items_property_only_shows_todo_items_and_not_anything_else():
     assert todo_item.title == "A Test Todo"
     assert todo_item.status == "To Do"
 
+
+def test_done_items_property_only_shows_done_items_and_not_anything_else():
+    #Arrange -  Create an instance of ViewModel containing some example Item objects with various statuses
+    items = [
+        Item("1", "A Test Todo", "To Do"),
+        Item("2", "An in progress Todo", "Doing"),
+        Item("3", "A completed Todo", "Done")
+    ]
+
+    view_model = ViewModel(items)
+
+    #Act -  Get the result of the view model's new done_items property (though this will actually be an empty list for now).
+    test_done_items = view_model.done_items
+
+    assert len(test_done_items) == 1
+
+    todo_item = test_done_items[0]
+
+    assert todo_item.id == "1"
+    assert todo_item.title == "A Test Todo"
+    assert todo_item.status == "To Do"
