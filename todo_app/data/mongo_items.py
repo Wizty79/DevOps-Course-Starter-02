41d801c3c089712123d89d02e8068a9d843abe2c
Wizty79 <wistikwistisen@gmail.com>
2022-09-08 14:30:42 +0000

    adding possible options to mongo_items

--- todo_app/data/mongo_items.py
+++ todo_app/data/mongo_items.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
+from azure.cosmos import cosmos_client #? possible solution
+import json #? possible solution
 import requests
 import os
 
 def get_mongo_db():
     url = PRIMARY_CONNECTION_STRING_DB
     
+    querystring = {
+        "USERNAME":os.getenv("USERNAME"),
+        "PASSWORD":os.getenv("PRIMARY_PASSWORD"),
+        "HOST":os.getenv("HOST_DB"),
+        "CORRELATIONID":os.getenv("CORRELATION_ID_DB")
+    }
+
