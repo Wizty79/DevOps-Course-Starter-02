41f267944733397de041d69cc6f8b0e79f5c5741
Wizty79 <wistikwistisen@gmail.com>
2022-09-12 14:12:14 +0000

    fixing indentation in mongo items v2

--- todo_app/data/mongo_items_Two.py
+++ todo_app/data/mongo_items_Two.py
@@ -6,21 +6,18 @@ import os
 import certifi #? possible solution
 
 def connect_mongo_db():
-    
-    
     client = pymongo.MongoClient(PRIMARY_CONNECTION_STRING_DB)
-try:
-    client.server_info() # validate connection string
-except pymongo.errors.ServerSelectionTimeoutError:
-    raise TimeoutError("Invalid API for MongoDB connection string or timed out when attempting to connect")
+    try:
+        client.server_info() # validate connection string
+    except pymongo.errors.ServerSelectionTimeoutError:
+        raise TimeoutError("Invalid API for MongoDB connection string or timed out when attempting to connect")
 
 
 def create_mongo_todo_item():
-    document_id = collection.insert_one({Item(card['id'], card['name'], list['name']}).inserted_id
+    document_id = collection.insert_one({Item(card['id'], card['name'], list['name'])}).inserted_id
 
 
 def change_mongo_status():
-
     collection.update_one({"_id": document_id}, {"$set":{SAMPLE_FIELD_NAME: "Updated!"}})
     print("Updated document with _id {}: {}".format(document_id, collection.find_one({"_id": document_id})))
 
