5ec7accb18a1bbbab6593f81593b1fce40e45c83
Wizty79 <wistikwistisen@gmail.com>
2022-09-12 13:46:38 +0000

    adding more sample functions to second mongo items

--- todo_app/data/mongo_items_Two.py
+++ todo_app/data/mongo_items_Two.py
@@ -5,7 +5,7 @@ import requests
 import os
 import certifi #? possible solution
 
-def get_mongo_db():
+def connect_mongo_db():
     
     
     client = pymongo.MongoClient(PRIMARY_CONNECTION_STRING_DB)
@@ -13,3 +13,16 @@ try:
     client.server_info() # validate connection string
 except pymongo.errors.ServerSelectionTimeoutError:
     raise TimeoutError("Invalid API for MongoDB connection string or timed out when attempting to connect")
+
+
+def create_mongo_todo_item():
+    document_id = collection.insert_one({Item(card['id'], card['name'], list['name']}).inserted_id
+
+
+def change_mongo_status():
+
+    collection.update_one({"_id": document_id}, {"$set":{SAMPLE_FIELD_NAME: "Updated!"}})
+    print("Updated document with _id {}: {}".format(document_id, collection.find_one({"_id": document_id})))
+
+
+#all need to be adapted! How? 
