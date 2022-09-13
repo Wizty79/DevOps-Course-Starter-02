550cef817a2347c6a2af51311d4791944b98cbf9
Wizty79 <wistikwistisen@gmail.com>
2022-09-13 09:22:28 +0000

    building mongo todo and done functions

--- todo_app/data/mongo_items_Vtwo.py
+++ todo_app/data/mongo_items_Vtwo.py
@@ -11,7 +11,7 @@ todos = db.ToDoItems #collection for todo items
 dones = db.DoneItems #collection for done items
 
 def connect_mongo_db():
-    client = pymongo.MongoClient(PRIMARY_CONNECTION_STRING_DB) #is this line need?
+    #client = pymongo.MongoClient(PRIMARY_CONNECTION_STRING_DB) #is this line need?
     
     response = requests.get(client)
 
@@ -21,12 +21,20 @@ def connect_mongo_db():
 
 
 def create_mongo_todo_item():
-    document_id = collection.insert_one({Item(card['id'], card['name'], list['name'])}).inserted_id
+    new_todo_title = request.form['todo-name']
+    
+    document_id = todos.insert_one({Item(card['id'], card['name'], list['name'])}).inserted_id 
+    #move documnent_id out of function and into root of file? 
+    
+    response = requests.request(client, document_id)
 
 
 def change_mongo_status():
-    collection.update_one({"_id": document_id}, {"$set":{SAMPLE_FIELD_NAME: "Updated!"}})
-    print("Updated document with _id {}: {}".format(document_id, collection.find_one({"_id": document_id})))
+    card_id = request.form['todo-id']
+    
+    collection = dones.update_one({"_id": document_id}, {"$set":{SAMPLE_FIELD_NAME: "Updated!"}})
+    
+    response = requests.request(client, collection)
 
 
 
