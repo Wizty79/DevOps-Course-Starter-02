50182ecfd91f455857e95f6f6275f9e6eabf48a7
Wizty79 <wistikwistisen@gmail.com>
2022-09-15 13:12:18 +0000

    adding notes

--- todo_app/data/mongo_items_Vtwo.py
+++ todo_app/data/mongo_items_Vtwo.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
-from azure.cosmos import cosmos_client #? possible solution
-import json #? possible solution
+from azure.cosmos import cosmos_client #? possibly need
+import json #? possibly need 
 import requests
 import os
-import certifi #? possible solution
+import certifi #? possibly need
 
 client = pymongo.MongoClient(PRIMARY_CONNECTION_STRING_DB)
 db = client.chaostodo-db #my database name
-document_id = todos.insert_one({Item(card['id'], card['name'], list['name'])}).inserted_id 
+document_id = todos.insert_one({Item(card['id'], card['name'], list['name'])}).inserted_id # replace card, but with what?
 todos = db.ToDoItems #collection for todo items
 dones = db.DoneItems #collection for done items
 
@@ -31,7 +31,7 @@ def change_mongo_status():
     
     card_id = request.form['todo-id']
     
-    change_status = dones.update_one({"_id": document_id}, {"$set":{SAMPLE_FIELD_NAME: "Updated!"}})
+    change_status = dones.update_one({"_id": document_id}, {"$set":{SAMPLE_FIELD_NAME: "Updated!"}}) 
     
     response = requests.request(client, change_status)
 
