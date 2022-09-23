1d3b942843e32fcf790f070889c1770ca9a94902
Wizty79 <wistikwistisen@gmail.com>
2022-09-23 08:17:24 +0000

    adding note, removing un-needed import lines

--- todo_app/data/mongo_items_Vtwo.py
+++ todo_app/data/mongo_items_Vtwo.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template, request
-#from azure.cosmos import cosmos_client 
 from pymongo import MongoClient
-#import json #? possibly need 
 import requests
 import os
-#import certifi #? possibly need
 
 client = MongoClient(r"PRIMARY_CONNECTION_STRING_DB")
 db = client.chaostododb #my database name
 todos = db.ToDoItems #collection for todo items
 dones = db.DoneItems #collection for done items
-#document_id = todos.insert_one({"_id": document_id}).inserted_id
-#document_id_dones = dones.insert_one({Item(card['id'], card['name'], list['name'])}).inserted_id # replace card, but with what?
 
 def connect_mongo_db():
     
@@ -33,10 +28,10 @@ def create_mongo_todo_item():
 def change_mongo_status():
     #document_id = dones.insert_one({"_id": document_id}).inserted_id
     
-    card_id = request.form['todo-id'] #replace card with _id? 
+    card_id = request.form['todo-id']
     
     #change_status = dones.update_one({"_id": document_id}, {"$set":{SAMPLE_FIELD_NAME: "Updated!"}}) 
-    change_status = dones.update_one(update)
+    change_status = dones.update_one(update) #2 parameters needed, a filter to select correct item and and update directory
                                      
     response = requests.request("PUT", client, change_status)
 
