2bca019c8d35d9a7b83d4a87bb198d74e978a20b
Wizty79 <wistikwistisen@gmail.com>
2022-09-26 14:49:40 +0000

    removing un-needed lines

--- todo_app/data/mongo_items_Vtwo.py
+++ todo_app/data/mongo_items_Vtwo.py
@@ -5,16 +5,8 @@ import requests
 import os
 
 client = MongoClient(os.getenv("PRIMARY_CONNECTION_STRING_DB"))
-db = client.chaostodoSaved #my database name
-todos = db.ToDoItems #collection for todo items
-#ones = db.DoneItems #collection for done items
-
-#A common task in web applications is to get an ObjectId from the request URL and find the matching document. 
-#It’s necessary in this case to convert the ObjectId from a string before passing it to find_one:
-#The web framework gets post_id from the URL and passes it as a string
-def get(post_id):
-    # Convert from string to ObjectId:
-    document = client.db.collection.find_one({'_id': ObjectId(post_id)})
+db = client.chaostodoSaved
+todos = db.ToDoItems
 
 def get_mongo_items():
     
@@ -25,7 +17,6 @@ def create_mongo_todo_item():
     todos.insert_one({"name": request.form['todo-name'], "status": "To Do"})
 
 def change_mongo_status():
-    #document_id = dones.insert_one({"_id": document_id}).inserted_id
     
     card_id = request.form['todo-id']
     
