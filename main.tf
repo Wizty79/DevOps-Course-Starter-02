ac760d5dd11ea9ee302de97cd1a3c5c31a2e6748
Wizty79 <wistikwistisen@gmail.com>
2022-11-22 12:17:52 +0000

    starting step 4

--- main.tf
+++ main.tf
@@ -37,3 +37,62 @@ resource "azurerm_linux_web_app" "main" {
  "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io" 
  }
 }
+
+##https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/cosmosdb_account
+##to be adapted
+
+resource "azurerm_resource_group" "main" {
+  name     = data.azurerm_resource_group.main.name
+  location = "UK South" 
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "tfex-cosmos-db-${random_integer.ri.result}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+  
+  capabilities { 
+	name = "EnableServerless" 
+  }
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = "westus"
+    failover_priority = 0
+  }
+}
