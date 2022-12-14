a73f6eee8ed0e23aac8778b3beaf809a8a948119
Wizty79 <wistikwistisen@gmail.com>
2022-12-14 12:22:58 +0000

    removing notes old notes

--- main.tf
+++ main.tf
@@ -29,17 +29,14 @@ resource "azurerm_service_plan" "main" {
   os_type             = "Linux"
   sku_name            = "B1"
 }
-resource "azurerm_linux_web_app" "main" {  #to be replaced with azurerm_app_service ?
-  name                = "TerraBerra-Chaos-Todo" #change to AZ app name "chaostododb"? already used under "azurerm_cosmosdb_account" ?
+resource "azurerm_linux_web_app" "main" { 
+  name                = "TerraBerra-Chaos-Todo"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   service_plan_id     = azurerm_service_plan.main.id
   site_config {
     application_stack {
-      #docker_image = "appsvcsample/python-helloworld"
-      #docker_image     = "wizty79/wi79_images"
       docker_image     = "wizty79/wi79_images"
-
       docker_image_tag = "latest"
     }
   }
@@ -56,9 +53,7 @@ resource "azurerm_linux_web_app" "main" {  #to be replaced with azurerm_app_serv
 
 resource "azurerm_cosmosdb_account" "db" {
   name = "chaos-cosmos-db"
-  #location            = azurerm_resource_group.example.location
   location = "UK South"
-  #resource_group_name = azurerm_resource_group.example.name
   resource_group_name = data.azurerm_resource_group.main.name
   offer_type          = "Standard"
   kind                = "MongoDB"
@@ -101,13 +96,9 @@ resource "azurerm_cosmosdb_account" "db" {
 }
 
 resource "azurerm_cosmosdb_mongo_database" "main" {
-  #name                = "tfex-cosmos-mongo-db"
   name = azurerm_cosmosdb_account.db.name
-  #resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
   resource_group_name = data.azurerm_resource_group.main.name
-  #account_name        = data.azurerm_cosmosdb_account.example.name
   account_name = azurerm_cosmosdb_account.db.name
-  #throughput   = 400
 
 }
 
